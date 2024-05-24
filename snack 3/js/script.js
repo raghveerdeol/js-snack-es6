1c65b3f6136870fba51254ee4fb8e87d57157cc7
raghveerdeol <deolraghveer5@gmail.com>
2024-05-24 18:07:36 +0200

    creata funzione minore

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -28,23 +28,25 @@ const bici = [
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare in console la bici con peso minore utilizzando destructuring e template literal --> */
-const [
-    bici1,
-    bici2,
-    bici3,
-    bici4,
-    bici5,
-    bici6,
-] = bici;
-console.log(bici3);
-
-
-for (const key in bici) {
-    
-    };
-
-bici.forEach((bike, index) => {
+let biciclette = [];
+bici.forEach((bike) => {
     // console.log(bike.peso);
-    const [peso] = bici;
-    console.log(peso)
-})
+    const {peso} = bike;
+    console.log(peso);
+    biciclette.push(peso);
+});
+const biciLeggera = minore(biciclette);
+console.log(biciclette);
+console.log(biciLeggera)
+
+function minore(dato) {
+    let x = dato[0];
+    for (let index = 0; index < dato.length; index++) {
+        if (x > dato[index]) {
+            x = dato[index];
+        } else {
+            x = x;
+        }
+    }
+    return x;
+}
