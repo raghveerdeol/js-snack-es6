950a2402f230417a697a7670485daccdcbdcd566
raghveerdeol <deolraghveer5@gmail.com>
2024-05-26 17:27:13 +0200

    aggiunto position con hetRandomCharacters

--- snack 5/js/script.js
+++ snack 5/js/script.js
@@ -17,11 +17,19 @@ let articoliModa = [
         color:  "pink",
     },
 ];
+
+
+const articoliPosition = articoliModa.map((articolo) => {
+    articolo.position = getRandomCharacters();
+    return articolo;
+});
+console.log(articoliPosition);
+
 function getRandomCharacters() {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         result = characters.charAt(Math.floor(Math.random() * characters.length));
     return result;
 };
-let x = getRandom();
+let x = getRandomCharacters();
 console.log(x);
